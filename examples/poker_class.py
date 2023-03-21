1c5192b2f8b6790aa26fb60a45ec4c3305225406
Ramzai <porfirev.o@gmail.com>
2023-03-21 23:00:13 +0700

    del asserts, function hend move from main

--- examples/poker_class.py
+++ examples/poker_class.py
@@ -2,7 +2,6 @@ import itertools
 from art import tprint
 
 
-
 class Card:
     def __init__(self, input_: str):
         self.card_ = input_
@@ -116,32 +115,9 @@ class Combination:
         return res
 
 
-if __name__ == '__main__':
-    def hend(hend, community):
-        c = Combination(hend + community)
-        return c.best_hand()
+def hend(hend, community):
+    c = Combination(hend + community)
+    return c.best_hand()
+# if __name__ == '__main__':
 
-
-    assert hend(['K♠', 'A♠'], ['Q♠', '5♦', '10♠', 'J♠', '8♦']) == ('straight-flush', ['A', 'K', 'Q', 'J', '10'])
-    assert hend(['K♠', 'J♠'], ['K♣', 'K♦', '9♦', 'K♥', '8♦']) == ('four-of-a-kind', ['K', 'J'])
-    assert hend(['9♠', '4♠'], ['9♣', '9♦', 'K♦', '9♥', '8♦']) == ('four-of-a-kind', ['9', 'K'])
-    assert hend(['K♠', '4♠'], ['K♣', 'K♦', '9♦', '8♥', '8♦']) == ('full house', ['K', '8'])
-    assert hend(['K♠', '4♠'], ['8♣', 'K♦', '9♦', '8♥', '8♦']) == ('full house', ['8', 'K'])
-    assert hend(['K♠', '4♠'], ['8♠', 'K♦', '9♠', '8♥', '7♠']) == ('flush', ['K', '9', '8', '7', '4'])
-    assert hend(['K♠', '4♠'], ['8♠', 'K♦', '9♠', 'J♥', '7♠']) == ('flush', ['K', '9', '8', '7', '4'])
-    assert hend(['A♣', '10♠'], ['8♠', 'Q♦', '9♠', 'J♣', '7♠']) == ('straight', ['Q', 'J', '10', '9', '8'])
-    assert hend(['A♣', '10♠'], ['8♠', 'Q♦', '9♠', 'J♣', 'K♠']) == ('straight', ['A', 'K', 'Q', 'J', '10'])
-    assert hend(['6♣', '10♠'], ['2♠', '3♦', '4♠', 'J♣', '5♠']) == ('straight', ['6', '5', '4', '3', '2'])
-    assert hend(['6♣', '10♠'], ['2♠', '6♦', '6♠', 'J♣', '5♠']) == ('three-of-a-kind', ['6', 'J', '10'])
-    assert hend(['2♣', '9♠'], ['2♠', '2♦', '4♠', 'Q♣', '5♠']) == ('three-of-a-kind', ['2', 'Q', '9'])
-    assert hend(['2♣', '9♠'], ['2♠', '4♦', '4♠', 'Q♣', '5♠']) == ('two pair', ['4', '2', 'Q'])
-    assert hend(['2♣', 'Q♠'], ['5♠', '2♦', '4♠', 'Q♣', '5♠']) == ('two pair', ['Q', '5', '4'])
-    assert hend(['2♣', 'Q♠'], ['3♠', '2♦', '4♠', 'J♣', '5♠']) == ('pair', ['2', 'Q', 'J', '5'])
-    assert hend(['2♣', 'Q♠'], ['3♠', '7♦', '4♠', 'Q♣', '6♠']) == ('pair', ['Q', '7', '6', '4'])
-    assert hend(['A♣', '9♠'], ['3♠', '7♦', '4♠', 'Q♣', 'K♠']) == ('nothing', ['A', 'K', 'Q', '9', '7'])
-    assert hend(['2♣', 'Q♠'], ['3♠', '7♦', '4♠', '10♣', '6♠']) == ('nothing', ['Q', '10', '7', '6', '4'])
-    assert hend(['K♠', 'J♠'], ['Q♠', '10♠', '9♦', 'A♠', 'J♠']) == ('straight-flush', ['A', 'K', 'Q', 'J', '10'])
-    assert hend(['K♠', 'J♥'], ['Q♦', '10♦', '9♦', 'A♠', 'Q♣']) == ('straight', ['A', 'K', 'Q', 'J', '10'])
-    assert hend(['K♠', 'J♥'], ['5♦', 'K♦', '9♦', 'A♠', 'Q♣']) == ('pair', ['K', 'A', 'Q', 'J'])
-
-    tprint("OHO      PAbOTAET")
+# tprint("OHO      PAbOTAET")
